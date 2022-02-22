925a928365e438dd289269025cd72b58cc1d125a
Lê Minh Mỹ <50837444+leminhmy@users.noreply.github.com>
2022-02-22 13:40:46 +0700

    add APi-controller recommendedProduct

---
+++ lib/data/repository/recommended_product_repo.dart
@@ -0,0 +1,12 @@
+import 'package:food_ui/data/api/api_client.dart';
+import 'package:food_ui/utils/app_contants.dart';
+import 'package:get/get.dart';
+
+class RecommendedProductRepo extends GetxService{
+  final ApiClient apiClient;
+  RecommendedProductRepo({required this.apiClient});
+
+  Future<Response> getRecommendedProductList() async{
+    return await apiClient.getData(AppConstants.RECOMMENDED_PRODUCT_URI);
+  }
+}
